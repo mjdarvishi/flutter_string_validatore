ddcc95f2c68f1a27aecb280a6d780c03001a4cc0
Imamul Akhyar <imamulakhyar93@gmail.com>
2023-02-08 21:17:47 +0700

    Allow all rules message to be overriden

--- lib/validation/validation.dart
+++ lib/validation/validation.dart
@@ -5,8 +5,8 @@ import 'package:strings_validator/validation/messages.dart';
 
 //main class for all validations
 class MainValidator {
-  Map<String,String>? messages;
-  Message errors=Message();
+  Map<String, String>? messages;
+  Message errors = Message();
   //if message pass to method it will override
   MainValidator(Map? messages) {
     if (messages != null) {
@@ -21,11 +21,20 @@ class MainValidator {
           case 'max':
             errors.maxLength = param.value;
             break;
-            case 'number':
+          case 'number':
             errors.number = param.value;
             break;
-            case 'alpha':
+          case 'alpha':
             errors.alpha = param.value;
+            break;
+          case 'email':
+            errors.email = param.value;
+            break;
+          case 'upperCase':
+            errors.existUpperCase = param.value;
+            break;
+          case 'specialChar':
+            errors.existSepChar = param.value;
             break;
         }
       }
@@ -35,7 +44,7 @@ class MainValidator {
 
 //abstract class with provide a basic class for all kind of out validation
 abstract class Validator extends MainValidator {
-  Validator( messages) : super(messages);
+  Validator(messages) : super(messages);
 
   String? validate(String input);
 }
@@ -43,106 +52,111 @@ abstract class Validator extends MainValidator {
 //check max length
 class MaxLengthValidator extends Validator {
   int size;
-  MaxLengthValidator(messages,{required this.size}):super(messages);
+  MaxLengthValidator(messages, {required this.size}) : super(messages);
 
   @override
   String? validate(String input) {
-    try{
-
-      if (input.length>size) {
+    try {
+      if (input.length > size) {
         throw LengthFormatException(errors.maxLength);
       }
     } on BaseException catch (e) {
       return e.message;
     }
+    return null;
   }
 }
 
 //check min length
 class MinLengthValidator extends Validator {
   int size;
-  MinLengthValidator(messages,{required this.size}):super(messages);
+  MinLengthValidator(messages, {required this.size}) : super(messages);
 
   @override
   String? validate(String input) {
-    try{
-      if (input.length<size) {
-        throw  LengthFormatException(errors.minLength);
+    try {
+      if (input.length < size) {
+        throw LengthFormatException(errors.minLength);
       }
     } on BaseException catch (e) {
       return e.message;
     }
+    return null;
   }
 }
 
 //check empty value
-class RequiredValidator extends Validator{
+class RequiredValidator extends Validator {
   RequiredValidator(messages) : super(messages);
 
   @override
   String? validate(String input) {
-    try{
+    try {
       if (input.isEmpty) {
-        throw  EmptyException(errors.empty);
+        throw EmptyException(errors.empty);
       }
-    }on BaseException catch (e) {
+    } on BaseException catch (e) {
       return e.message;
     }
+    return null;
   }
-
 }
 
 //check number format
-class NumberValidator extends Validator{
-  NumberValidator( messages) : super(messages);
+class NumberValidator extends Validator {
+  NumberValidator(messages) : super(messages);
 
   @override
   String? validate(String input) {
-    try{
+    try {
       if (!RegExp(r'^-?[0-9]+$').hasMatch(input)) {
         throw NumberException(errors.number);
       }
-    }on BaseException catch (e) {
+    } on BaseException catch (e) {
       return e.message;
     }
+    return null;
   }
-
 }
 
 //check alphabet format
-class AlphabetValidator extends Validator{
+class AlphabetValidator extends Validator {
   AlphabetValidator(messages) : super(messages);
 
   @override
   String? validate(String input) {
-    try{
+    try {
       if (!RegExp(r'^[a-zA-Z_]*$').hasMatch(input)) {
         throw AlphabetException(errors.alpha);
       }
-    }on BaseException catch (e) {
+    } on BaseException catch (e) {
       return e.message;
     }
+    return null;
   }
 }
 
 //check email format
 class EmailValidator extends Validator {
-  EmailValidator( messages) : super(messages);
+  EmailValidator(messages) : super(messages);
 
   @override
   String? validate(String input) {
     try {
-      if (!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(input)) {
-        throw FormatException(errors.invalidFormat);
+      if (!RegExp(
+              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+          .hasMatch(input)) {
+        throw FormatException(errors.email);
       }
     } on BaseException catch (e) {
       return e.message;
     }
+    return null;
   }
 }
 
 //check upper case
-class UpperCaseValidator extends Validator{
+class UpperCaseValidator extends Validator {
   UpperCaseValidator(messages) : super(messages);
 
   @override
@@ -150,30 +164,22 @@ class UpperCaseValidator extends Validator{
     if (!RegExp(r'^(?=.*?[A-Z])').hasMatch(input)) {
       throw UpperCasePasswordException(errors.existUpperCase);
     }
+    return null;
   }
-
 }
 
-
-
 //check special chat in string
-class SpecialCharacterValidator extends Validator{
+class SpecialCharacterValidator extends Validator {
   SpecialCharacterValidator(messages) : super(messages);
 
   @override
   String? validate(String input) {
-
-      if (!RegExp(r'^(?=.*?[!@#\$&*~])').hasMatch(input)) {
-        throw SepCharacterPasswordException(errors.existSepChar);
+    if (!RegExp(r'^(?=.*?[!@#\$&*~])').hasMatch(input)) {
+      throw SepCharacterPasswordException(errors.existSepChar);
     }
+    return null;
   }
-
 }
-
-
-
-
-
 
 //validation for string
 //max:3
@@ -185,42 +191,49 @@ class SpecialCharacterValidator extends Validator{
 //upperCase
 //specialChar
 
-
 //main method for validating string
 extension StringValidation on String {
-  String? validate(String param,{Map<String,String>?messages}) {
-    String?errMessage;
-    List<String>params=param.split('|').reversed.toList();
-    for(var item in params){
-      if(!item.contains(':')){
-        switch(item){
+  String? validate(String param, {Map<String, String>? messages}) {
+    String? errMessage;
+    List<String> params = param.split('|').reversed.toList();
+    for (var item in params) {
+      if (!item.contains(':')) {
+        switch (item) {
           case 'required':
-            errMessage= RequiredValidator(messages).validate(this)??errMessage;
+            errMessage =
+                RequiredValidator(messages).validate(this) ?? errMessage;
             break;
           case 'number':
-            errMessage=NumberValidator(messages).validate(this)??errMessage;
-          break;
+            errMessage = NumberValidator(messages).validate(this) ?? errMessage;
+            break;
           case 'alpha':
-            errMessage=AlphabetValidator(messages).validate(this)??errMessage;
-          break;
+            errMessage =
+                AlphabetValidator(messages).validate(this) ?? errMessage;
+            break;
           case 'email':
-            errMessage=EmailValidator(messages).validate(this)??errMessage;
-          break;
+            errMessage = EmailValidator(messages).validate(this) ?? errMessage;
+            break;
           case 'upperCase':
-            errMessage=UpperCaseValidator(messages).validate(this)??errMessage;
-          break;
+            errMessage =
+                UpperCaseValidator(messages).validate(this) ?? errMessage;
+            break;
           case 'specialChar':
-            errMessage=SpecialCharacterValidator(messages).validate(this)??errMessage;
-          break;
+            errMessage = SpecialCharacterValidator(messages).validate(this) ??
+                errMessage;
+            break;
         }
-      }else{
-        List<String>items=item.split(':');
-        switch(items[0]){
+      } else {
+        List<String> items = item.split(':');
+        switch (items[0]) {
           case 'max':
-            errMessage= MaxLengthValidator(messages,size:int.parse(items[1])).validate(this)??errMessage;
+            errMessage = MaxLengthValidator(messages, size: int.parse(items[1]))
+                    .validate(this) ??
+                errMessage;
             break;
           case 'min':
-            errMessage= MinLengthValidator(messages,size:int.parse(items[1])).validate(this)??errMessage;
+            errMessage = MinLengthValidator(messages, size: int.parse(items[1]))
+                    .validate(this) ??
+                errMessage;
             break;
         }
       }
